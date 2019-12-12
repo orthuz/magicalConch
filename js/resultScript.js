89513f3d0262e8538e250a156dcd0be15c30f622
orthuz <blexcat1000@gmail.com>
2019-12-12 16:52:04 +0900

    Update resultScript.js

--- js/resultScript.js
+++ js/resultScript.js
@@ -1,4 +1,14 @@
+var src;
+
 function result(){
   var result = Math.floor(Math.random()*3);
+  
+  if(result == 0){
+    src="./src/soraNo.jpg";
+  }else if(result == 1){
+    src="./src/soraNothing";
+  }else if(result == 2){
+    src="./src/soraYes";
+  }
   return result;
 }
